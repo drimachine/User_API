7f194f8c9954545d9e2d7ec8f74c0fd7d9be3131
Pedro Henrique <phesperanca07@gmail.com>
2026-04-29 08:43:49 -0300

    seed users

--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
 from database import SessionLocal 
-from models import Role 
+from models import Role, User
 
 
 session = SessionLocal()
@@ -12,7 +12,14 @@ try:
         Role(name="user")
     ]
 
+    users = [
+          User(name="Pedro", email="pedro@email.com", role_id=1),
+          User(name="Matheus", email="matheus@email.com", role_id=2)
+    ]
+
     session.add_all(roles)
+    session.commit()
+    session.add_all(users)
     session.commit()
 finally:
         session.close()
